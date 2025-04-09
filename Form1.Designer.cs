b73cffe85b03905885ca7a29986e578b347bfe2c
Stephanie Echavez <162314552+StephanieEchavez@users.noreply.github.com>
2025-04-09 11:40:59 +0800

    Create Form1.Designer.cs

---
+++ Form1.Designer.cs
@@ -0,0 +1,274 @@
+namespace StudentRecordApp
+{
+    partial class StudentInfoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            txtIDNumber = new TextBox();
+            txtFirstName = new TextBox();
+            txtLastName = new TextBox();
+            txtMiddleName = new TextBox();
+            txtCourse = new TextBox();
+            txtYearLevel = new TextBox();
+            btnSave = new Button();
+            btnClear = new Button();
+            label8 = new Label();
+            txtBirthday = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(40, 112);
+            label1.Name = "label1";
+            label1.Size = new Size(95, 20);
+            label1.TabIndex = 0;
+            label1.Text = "ID Number:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(40, 169);
+            label2.Name = "label2";
+            label2.Size = new Size(97, 20);
+            label2.TabIndex = 1;
+            label2.Text = "First Name:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(40, 225);
+            label3.Name = "label3";
+            label3.Size = new Size(96, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Last Name:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(40, 281);
+            label4.Name = "label4";
+            label4.Size = new Size(112, 20);
+            label4.TabIndex = 3;
+            label4.Text = "Middle Name:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(40, 338);
+            label5.Name = "label5";
+            label5.Size = new Size(68, 20);
+            label5.TabIndex = 4;
+            label5.Text = "Course:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.Location = new Point(40, 394);
+            label6.Name = "label6";
+            label6.Size = new Size(93, 20);
+            label6.TabIndex = 5;
+            label6.Text = "Year Level:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.Location = new Point(40, 450);
+            label7.Name = "label7";
+            label7.Size = new Size(76, 20);
+            label7.TabIndex = 6;
+            label7.Text = "Birthday:";
+            // 
+            // txtIDNumber
+            // 
+            txtIDNumber.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtIDNumber.Location = new Point(170, 109);
+            txtIDNumber.Margin = new Padding(3, 4, 3, 4);
+            txtIDNumber.Name = "txtIDNumber";
+            txtIDNumber.Size = new Size(300, 26);
+            txtIDNumber.TabIndex = 7;
+            // 
+            // txtFirstName
+            // 
+            txtFirstName.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtFirstName.Location = new Point(170, 165);
+            txtFirstName.Margin = new Padding(3, 4, 3, 4);
+            txtFirstName.Name = "txtFirstName";
+            txtFirstName.Size = new Size(300, 26);
+            txtFirstName.TabIndex = 8;
+            // 
+            // txtLastName
+            // 
+            txtLastName.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtLastName.Location = new Point(170, 221);
+            txtLastName.Margin = new Padding(3, 4, 3, 4);
+            txtLastName.Name = "txtLastName";
+            txtLastName.Size = new Size(300, 26);
+            txtLastName.TabIndex = 9;
+            // 
+            // txtMiddleName
+            // 
+            txtMiddleName.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtMiddleName.Location = new Point(170, 278);
+            txtMiddleName.Margin = new Padding(3, 4, 3, 4);
+            txtMiddleName.Name = "txtMiddleName";
+            txtMiddleName.Size = new Size(300, 26);
+            txtMiddleName.TabIndex = 10;
+            // 
+            // txtCourse
+            // 
+            txtCourse.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtCourse.Location = new Point(170, 334);
+            txtCourse.Margin = new Padding(3, 4, 3, 4);
+            txtCourse.Name = "txtCourse";
+            txtCourse.Size = new Size(300, 26);
+            txtCourse.TabIndex = 11;
+            // 
+            // txtYearLevel
+            // 
+            txtYearLevel.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtYearLevel.Location = new Point(170, 390);
+            txtYearLevel.Margin = new Padding(3, 4, 3, 4);
+            txtYearLevel.Name = "txtYearLevel";
+            txtYearLevel.Size = new Size(300, 26);
+            txtYearLevel.TabIndex = 12;
+            // 
+            // btnSave
+            // 
+            btnSave.BackColor = SystemColors.HotTrack;
+            btnSave.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnSave.ForeColor = SystemColors.ButtonHighlight;
+            btnSave.Location = new Point(170, 512);
+            btnSave.Margin = new Padding(3, 4, 3, 4);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(140, 50);
+            btnSave.TabIndex = 14;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = false;
+            btnSave.Click += btnSave_Click;
+            // 
+            // btnClear
+            // 
+            btnClear.BackColor = Color.DarkRed;
+            btnClear.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnClear.ForeColor = SystemColors.ButtonHighlight;
+            btnClear.Location = new Point(330, 512);
+            btnClear.Margin = new Padding(3, 4, 3, 4);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(140, 50);
+            btnClear.TabIndex = 15;
+            btnClear.Text = "Clear";
+            btnClear.UseVisualStyleBackColor = false;
+            btnClear.Click += btnClear_Click;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Microsoft Sans Serif", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.Location = new Point(130, 38);
+            label8.Name = "label8";
+            label8.Size = new Size(270, 31);
+            label8.TabIndex = 16;
+            label8.Text = "Student Information";
+            // 
+            // txtBirthday
+            // 
+            txtBirthday.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtBirthday.Location = new Point(170, 446);
+            txtBirthday.Margin = new Padding(3, 4, 3, 4);
+            txtBirthday.Name = "txtBirthday";
+            txtBirthday.Size = new Size(300, 26);
+            txtBirthday.TabIndex = 13;
+            // 
+            // StudentInfoForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(532, 591);
+            Controls.Add(txtBirthday);
+            Controls.Add(label8);
+            Controls.Add(btnClear);
+            Controls.Add(btnSave);
+            Controls.Add(txtYearLevel);
+            Controls.Add(txtCourse);
+            Controls.Add(txtMiddleName);
+            Controls.Add(txtLastName);
+            Controls.Add(txtFirstName);
+            Controls.Add(txtIDNumber);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(3, 4, 3, 4);
+            MaximizeBox = false;
+            Name = "StudentInfoForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Student Record Form";
+            ResumeLayout(false);
+            PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtIDNumber;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtMiddleName;
+        private System.Windows.Forms.TextBox txtCourse;
+        private System.Windows.Forms.TextBox txtYearLevel;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtBirthday;
+    }
+}
